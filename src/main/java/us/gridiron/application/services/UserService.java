c939ee00bf75ad60d9934cff38806dafc28c200d
Josh Clemons <mrjoshc@gmail.com>
2023-07-27 09:51:24 -0500

    Added password validator

--- src/main/java/us/gridiron/application/services/UserService.java
+++ src/main/java/us/gridiron/application/services/UserService.java
@@ -10,33 +10,45 @@ import us.gridiron.application.repository.UserRepository;
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final CodeService codeService;
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final CodeService codeService;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, CodeService codeService) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.codeService = codeService;
-    }
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, CodeService codeService)
+	{
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.codeService = codeService;
+	}
 
-    public User getLoggedInUser() {
-        String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+	public User getLoggedInUser()
+	{
+		String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        return userRepository.findByUsername(loggedInUsername)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + loggedInUsername));
-    }
+		return userRepository.findByUsername(loggedInUsername)
+			.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + loggedInUsername));
+	}
 
-    public User resetPassword(String username, String newPassword, String accessCode) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+	public User resetPassword(String username, String newPassword, String accessCode)
+	{
+		User user = userRepository.findByUsername(username)
+			.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        codeService.validateCode(accessCode, 120);
+		codeService.validateCode(accessCode, 120);
 
-        String encodedPassword = passwordEncoder.encode(newPassword);
-        user.setPassword(encodedPassword);
+		String encodedPassword = passwordEncoder.encode(newPassword);
+		user.setPassword(encodedPassword);
 
-        return userRepository.save(user);
+		return userRepository.save(user);
+	}
 
-    }
+	/**
+	 * @param password
+	 * @return true if password is valid, false otherwise
+	 */
+	public boolean validatePassword(String password)
+	{
+		// This can be expanded if more password complexity is required, or better error responses
+		return password.length() > 5;
+	}
 }

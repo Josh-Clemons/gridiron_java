1886721bbe2e39071a3671433bfedeceaddc0a60
Josh Clemons <mrjoshc@gmail.com>
2023-08-04 13:39:03 -0500

    Added help email flow

---
+++ src/main/java/us/gridiron/application/payload/request/HelpEmailDTO.java
@@ -0,0 +1,22 @@
+package us.gridiron.application.payload.request;
+
+public class HelpEmailDTO {
+	private String email;
+	private String subject;
+	private String message;
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public String getSubject()
+	{
+		return subject;
+	}
+
+	public String getMessage()
+	{
+		return message;
+	}
+}

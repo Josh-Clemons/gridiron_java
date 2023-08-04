d9da9cc6da2d4abf44fd0bb0674101add0423c48
Josh Clemons <mrjoshc@gmail.com>
2023-08-04 13:39:03 -0500

    Added help email flow

--- src/main/java/us/gridiron/application/payload/request/InviteEmailDTO.java
+++ src/main/java/us/gridiron/application/payload/request/InviteEmailDTO.java
@@ -1,40 +1,48 @@
 package us.gridiron.application.payload.request;
 
-public class EmailDTO {
+public class InviteEmailDTO {
 	private String to;
 	private String subject;
 	private String text;
 	private String inviteCode;
 
-	public String getTo() {
+	public String getTo()
+	{
 		return to;
 	}
 
-	public void setTo(String to) {
+	public void setTo(String to)
+	{
 		this.to = to;
 	}
 
-	public String getSubject() {
+	public String getSubject()
+	{
 		return subject;
 	}
 
-	public void setSubject(String subject) {
+	public void setSubject(String subject)
+	{
 		this.subject = subject;
 	}
 
-	public String getText() {
+	public String getText()
+	{
 		return text;
 	}
 
-	public void setText(String text) {
+	public void setText(String text)
+	{
 		this.text = text;
 	}
 
-	public String getInviteCode() {
+	public String getInviteCode()
+	{
 		return inviteCode;
 	}
 
-	public void setInviteCode(String inviteCode) {
+	public void setInviteCode(String inviteCode)
+	{
 		this.inviteCode = inviteCode;
 	}
 }

ab312e99efb33bd23d728650bb33a6f23312fda7
Josh <mrjoshc@gmail.com>
2024-03-03 12:35:21 -0600

    Created property config so I can later separate queries from the repository

--- src/main/java/us/gridiron/application/configuration/PropertyConfig.java
+++ src/main/java/us/gridiron/application/configuration/PropertyConfig.java
@@ -1,2 +1,11 @@
-package us.gridiron.application.configuration;public class PropertyConfig {
+package us.gridiron.application.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:queries.properties")
+public class PropertyConfig
+{
 }
